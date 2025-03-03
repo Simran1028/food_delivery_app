720d109029cd302aa7fc520f786a611af6cca39e
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-04 01:34:35 +0530

    mongodb connection error fixed

--- src/app/api/restaurant/route.js
+++ src/app/api/restaurant/route.js
@@ -3,8 +3,8 @@ import { restaurantsSchema } from "@/app/lib/restaurantsModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    await mongoose.connect(connectionStr);
-    const data=await restaurantsSchema.find()
-return NextResponse.json({result:data})
+export async function GET() {
+  await mongoose.connect(connectionStr);
+  const data = await restaurantsSchema.find();
+  return NextResponse.json({ result: data });
 }
--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,4 @@
-const {username,password}=process.env
-export const connectionStr="mongodb+srv://"+username+":"+password+"@cluster0.ju8fk.mongodb.net/foodDeliveryDB?retryWrites=true&w=majority&appName=Cluster0"
+const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+export const connectionStr = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.ju8fk.mongodb.net/foodDeliveryDB?retryWrites=true&w=majority&appName=Cluster0`;
+
+
