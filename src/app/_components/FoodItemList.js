2e01490bfff84df022b3061a19d65915da5df329
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-12 23:56:16 +0530

    api added in food item list

--- src/app/_components/FoodItemList.js
+++ src/app/_components/FoodItemList.js
@@ -1,30 +1,59 @@
-const FoodItemList=()=>{
-return(
+import { useEffect, useState } from "react";
+
+const FoodItemList = () => {
+  const [foodItems, setFoodItems] = useState();
+
+  useEffect(() => {
+    loadFoodItems();
+  }, []);
+
+  let loadFoodItems = async () => {
+    let restaurantData = JSON.parse(localStorage.getItem("restaurantUser"));
+    let restro_id = restaurantData._id;
+    let response = await fetch(
+      "http://localhost:3000/api/restaurant/food/" + restro_id
+    );
+    response = await response.json();
+    if (response.success) {
+      setFoodItems(response.result);
+    } else {
+      alert("Food Item List not loading.");
+    }
+  };
+  return (
     <>
-    <h1>Food Item List</h1>
-    <table>
+      <h1>Food Item List</h1>
+      <table>
         <thead>
-            <tr>
-                <td>SNo.</td>
-                <td>Name</td>
-                <td>Price</td>
-                <td>Description</td>
-                <td>Image</td>
-                <td>Action</td>
-            </tr>
+          <tr>
+            <td>SNo.</td>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Description</td>
+            <td>Image</td>
+            <td>Action</td>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td>Burger</td>
-                <td>80</td>
-                <td>Delicious</td>
-                <td>image</td>
-                <td><button>Edit</button><button>Delete</button></td>
-            </tr>
+          {foodItems &&
+            foodItems.map((item, key) => (
+              <tr key={key}>
+                <td>{key + 1}</td>
+                <td>{item.foodName}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>
+                  <img src={item.imageURL}></img>
+                </td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
         </tbody>
-    </table>
+      </table>
     </>
-)
-}
+  );
+};
 export default FoodItemList;
