4048cfcd9066ea55998e088a3ce974ae5799e623
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-16 01:02:09 +0530

    main pg banner img added

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,15 @@ export default function Home() {
 
   return (
     <div>
-      <CustomerHeader/>
-      <h1>Food Delivery App</h1>
-      <RestaurantFooter/>
+      <CustomerHeader />
+      <div className="main-page-banner">
+        <h1>Food Delivery App</h1>
+        <div className="input-wrapper">
+          <input type="text" className="select-input" placeholder="Select Place "/>
+          <input type="text" className="search-input" placeholder="Enter food or Restaurant Name "/>
+        </div>
+      </div>
+      <RestaurantFooter />
     </div>
   );
 }
