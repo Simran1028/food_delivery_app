ef5c7bd1433d0550a3bae901097d8f3e84954e25
Simran1028 <simrandeepkaur06488@gmail.com>
2025-03-16 02:16:35 +0530

    apply location api

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,46 @@
+'use client'
+import { useEffect, useState } from "react";
 import CustomerHeader from "./_components/CustomerHeader";
 import RestaurantFooter from "./_components/RestaurantFooter";
 
 
 export default function Home() {
+  const [location, setLocation] = useState([]);
+  const [selectLocation, setSelectedLocation] = useState('');
+  const [showLocation, setShowLocation] = useState(false);
 
+  useEffect(() => {
+    loadLocation();
+  }, [])
+
+  const loadLocation = async () => {
+    let response = await fetch('http://localhost:3000/api/customer/location');
+    response = await response.json();
+    if (response.success) {
+      setLocation(response.result);
+    }
+  }
+
+  const handleItem = (item) => {
+    setSelectedLocation(item);
+    setShowLocation(false);
+  }
   return (
     <div>
       <CustomerHeader />
       <div className="main-page-banner">
         <h1>Food Delivery App</h1>
         <div className="input-wrapper">
-          <input type="text" className="select-input" placeholder="Select Place "/>
-          <input type="text" className="search-input" placeholder="Enter food or Restaurant Name "/>
+          <input type="text" className="select-input" value={selectLocation} readOnly
+            onClick={() => setShowLocation(true)} placeholder="Select Place " />
+          <ul className="location-list">
+            {
+              showLocation && location.map((item, index) => (
+                <li key={index} onClick={() => handleItem(item)}>{item}</li>
+              ))
+            }
+          </ul>
+          <input type="text" className="search-input" placeholder="Enter food or Restaurant Name " />
         </div>
       </div>
       <RestaurantFooter />
